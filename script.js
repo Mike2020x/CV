026ffc8a48d23512719f0a3eb49006b83569fe5c
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2022-08-17 19:14:37 -0500

    Portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+/************Menu**************/
+/*Esta funcion autoejecutable cambia el icono  y muestra y oculta el menu al hacer click */
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn");
+  $menu = d.querySelector(".menu");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
